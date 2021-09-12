862f7957d868a7c3e8d67746a7b6c59f7246b0bd
Rafael S. Zanella <rafaels.zanella@gmail.com>
2021-09-12 12:35:26 -0300

    chore: adicionar alias para imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +9,11 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+
+  /* https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391 */
+  webpackFinal: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/')
+    return config;
+  },
 }
